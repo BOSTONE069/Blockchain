2450d8da9a3894f15b8d65d9a9086591768a4727
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-02 13:47:19 -0400

    Added comments to the mintoken erc20

--- minttoken.sol
+++ minttoken.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+// This is are impoert from openzeppelin
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+
+// contract for the ERC20 token
 contract Web3Token is ERC20, AccessControl {
     constructor() ERC20("web3Token", "WCT") {
         _mint(msg.sender, 100000 * 10 ** decimals());
